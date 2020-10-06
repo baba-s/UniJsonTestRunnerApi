255e3e7d08992b2e7b131e85dd1546e505932d65
baba_s <shotababa1206@gmail.com>
2020-10-06 20:45:25 +0900

    first commit

---
+++ Editor/JsonITestAdaptor.cs
@@ -0,0 +1,57 @@
+﻿using JetBrains.Annotations;
+using System;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+
+namespace Kogane
+{
+	[Serializable]
+	public struct JsonITestAdaptor
+	{
+		[SerializeField][UsedImplicitly] private string   Id;
+		[SerializeField][UsedImplicitly] private string   ParentUniqueName;
+		[SerializeField][UsedImplicitly] private string   UniqueName;
+		[SerializeField][UsedImplicitly] private string   ParentFullName;
+		[SerializeField][UsedImplicitly] private string   ParentId;
+		[SerializeField][UsedImplicitly] private string   SkipReason;
+		[SerializeField][UsedImplicitly] private string   Description;
+		[SerializeField][UsedImplicitly] private RunState RunState;
+		[SerializeField][UsedImplicitly] private bool     IsTestAssembly;
+		[SerializeField][UsedImplicitly] private string[] Categories;
+		[SerializeField][UsedImplicitly] private int      TestCaseTimeout;
+		[SerializeField][UsedImplicitly] private bool     IsSuite;
+		[SerializeField][UsedImplicitly] private bool     HasChildren;
+		[SerializeField][UsedImplicitly] private int      TestCaseCount;
+		[SerializeField][UsedImplicitly] private string   FullName;
+		[SerializeField][UsedImplicitly] private string   Name;
+		[SerializeField][UsedImplicitly] private int      ChildIndex;
+		[SerializeField][UsedImplicitly] private TestMode TestMode;
+
+		public JsonITestAdaptor( ITestAdaptor other )
+		{
+			Id               = other.Id;
+			ParentUniqueName = other.ParentUniqueName;
+			UniqueName       = other.UniqueName;
+			ParentFullName   = other.ParentFullName;
+			ParentId         = other.ParentId;
+			SkipReason       = other.SkipReason;
+			Description      = other.Description;
+			RunState         = other.RunState;
+			IsTestAssembly   = other.IsTestAssembly;
+			Categories       = other.Categories;
+			TestCaseTimeout  = other.TestCaseTimeout;
+			IsSuite          = other.IsSuite;
+			HasChildren      = other.HasChildren;
+			TestCaseCount    = other.TestCaseCount;
+			FullName         = other.FullName;
+			Name             = other.Name;
+			ChildIndex       = other.ChildIndex;
+			TestMode         = other.TestMode;
+		}
+
+		public override string ToString()
+		{
+			return JsonUtility.ToJson( this, true );
+		}
+	}
+}
---
+++ Editor/JsonITestResultAdaptor.cs
@@ -0,0 +1,58 @@
+﻿using JetBrains.Annotations;
+using System;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+using TestStatus = UnityEditor.TestTools.TestRunner.Api.TestStatus;
+
+namespace Kogane
+{
+	[Serializable]
+	public struct JsonITestResultAdaptor
+	{
+		[SerializeField][UsedImplicitly] private string       Message;
+		[SerializeField][UsedImplicitly] private bool         HasChildren;
+		[SerializeField][UsedImplicitly] private int          InconclusiveCount;
+		[SerializeField][UsedImplicitly] private int          SkipCount;
+		[SerializeField][UsedImplicitly] private int          PassCount;
+		[SerializeField][UsedImplicitly] private int          FailCount;
+		[SerializeField][UsedImplicitly] private int          AssertCount;
+		[SerializeField][UsedImplicitly] private string       StackTrace;
+		[SerializeField][UsedImplicitly] private string       Output;
+		[SerializeField][UsedImplicitly] private string       StartTime;
+		[SerializeField][UsedImplicitly] private double       Duration;
+		[SerializeField][UsedImplicitly] private TestStatus   TestStatus;
+		[SerializeField][UsedImplicitly] private string       ResultState;
+		[SerializeField][UsedImplicitly] private string       FullName;
+		[SerializeField][UsedImplicitly] private string       Name;
+		[SerializeField][UsedImplicitly] private ITestAdaptor Test;
+		[SerializeField][UsedImplicitly] private string       EndTime;
+
+		public JsonITestResultAdaptor( ITestResultAdaptor other )
+		{
+			var jstZoneInfo = TimeZoneInfo.Local;
+
+			Message           = other.Message;
+			HasChildren       = other.HasChildren;
+			InconclusiveCount = other.InconclusiveCount;
+			SkipCount         = other.SkipCount;
+			PassCount         = other.PassCount;
+			FailCount         = other.FailCount;
+			AssertCount       = other.AssertCount;
+			StackTrace        = other.StackTrace;
+			Output            = other.Output;
+			StartTime         = TimeZoneInfo.ConvertTimeFromUtc( other.StartTime, jstZoneInfo ).ToString( "yyyy/MM/dd HH:mm:ss" );
+			Duration          = other.Duration;
+			TestStatus        = other.TestStatus;
+			ResultState       = other.ResultState;
+			FullName          = other.FullName;
+			Name              = other.Name;
+			Test              = other.Test;
+			EndTime           = TimeZoneInfo.ConvertTimeFromUtc( other.EndTime, jstZoneInfo ).ToString( "yyyy/MM/dd HH:mm:ss" );
+		}
+
+		public override string ToString()
+		{
+			return JsonUtility.ToJson( this, true );
+		}
+	}
+}
